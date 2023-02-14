a74f314c10f30c229da045701362d764f9ec601c
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-02-13 22:17:09 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'avatar-ai-dc-dev',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
